1d300afd22b5e6a09d2b18fb070312fab72c0a79
Nicolas Liampotis <nliam@grnet.gr>
2016-09-02 03:30:56 +0300

    Remove unused tables/columns from db migration scripts

--- oidc-server/src/main/resources/db/migration/V4___authenticating_authority.sql
+++ oidc-server/src/main/resources/db/migration/V4___authenticating_authority.sql
@@ -1,4 +1,4 @@
 --
 -- Some resource servers need the authenticating_authority
 --
-ALTER TABLE user_info ADD authenticating_authority varchar(256) DEFAULT NULL;
+ALTER TABLE user_info ADD authenticating_authority varchar(255) DEFAULT NULL;
