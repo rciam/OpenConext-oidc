9f10e4556687c96acae0029c5004a5c613c8f4a3
GOXR3PLUS <crAlexcomp@gmail.com>
2017-08-28 16:52:16 +0300

    Update 2

--- oidc-server/automation.bat
+++ oidc-server/automation.bat
@@ -4,11 +4,11 @@ if %1 == 1 (
 
 call mvn clean package
 
-rename C:\Users\GOXR3PLUS\Documents\OpenConext-oidc\oidc-server\target\oidc-server.war oidc.war
-pscp -i "C:/Users/GOXR3PLUS/.ssh/putty private key.ppk"  "C:\Users\GOXR3PLUS\Documents\OpenConext-oidc\oidc-server\target\oidc.war" root@snf-761523.vm.okeanos.grnet.gr:/var/lib/tomcat8/webapps/
+rename C:\Users\GOXR3PLUS\Desktop\GitHub\OpenConext-oidc\oidc-server\target\oidc-server.war oidc.war
+pscp -i "C:/Users/GOXR3PLUS/.ssh/putty private key.ppk"  "C:\Users\GOXR3PLUS\Desktop\GitHub\OpenConext-oidc\oidc-server\target\oidc.war" root@snf-761523.vm.okeanos.grnet.gr:/var/lib/tomcat8/webapps/
 
 )else (
 
-pscp -i "C:/Users/GOXR3PLUS/.ssh/putty private key.ppk"  "C:\Users\GOXR3PLUS\Documents\OpenConext-oidc\oidc-server\target\oidc.war" root@snf-761523.vm.okeanos.grnet.gr:/var/lib/tomcat8/webapps/
+pscp -i "C:/Users/GOXR3PLUS/.ssh/putty private key.ppk"  "C:\Users\GOXR3PLUS\Desktop\GitHub\OpenConext-oidc\oidc-server\target\oidc.war" root@snf-761523.vm.okeanos.grnet.gr:/var/lib/tomcat8/webapps/
 
 )
