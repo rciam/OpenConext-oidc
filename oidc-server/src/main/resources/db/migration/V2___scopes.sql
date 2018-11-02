7d1e2677df8b13c71884dc407cfea517b5287aa9
nikosev <nikos.ev@hotmail.com>
2018-11-02 10:46:05 +0200

    Fix syntax error in scope.sql

--- oidc-server/src/main/resources/db/migration/V2___scopes.sql
+++ oidc-server/src/main/resources/db/migration/V2___scopes.sql
@@ -6,7 +6,7 @@ INSERT INTO system_scope (scope, description, icon, restricted, default_scope, s
   ('openid', 'log in using your identity', 'user', false, true, false, null),
   ('profile', 'read your basic profile info', 'list-alt', true, true, false, null),
   ('email', 'read your email address', 'envelope', true, true, false, null),
-  ('offline_access', 'access your info while not being logged in', 'time', true, false, false, null);
-  ('eduperson_entitlement', 'read your rights to resources', 'bookmark', true, true, false, null);
-  ('eduperson_scoped_affiliation', 'read your affiliation within a domain', 'briefcase', true, true, false, null);
+  ('offline_access', 'access your info while not being logged in', 'time', true, false, false, null),
+  ('eduperson_entitlement', 'read your rights to resources', 'bookmark', true, true, false, null),
+  ('eduperson_scoped_affiliation', 'read your affiliation within a domain', 'briefcase', true, true, false, null),
   ('eduperson_unique_id', 'read your unique identifier', 'tag', true, true, false, null);
